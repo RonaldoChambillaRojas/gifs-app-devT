50407b47112b67f0a437330167a7cfcd7edcec65
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-21 23:10:15 -0500

    feat: (app) Add Search AND previous-searches

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -3,11 +3,24 @@ export const GifsApp = () => {
     <>
     {/* Header */}
 
-    <div>
+    <div className="content-center">
         <h1>Buscador de Gifs</h1>
         <p>Descubre y comparte el gif perfecto</p>
     </div>
-    
+    {/* Search */}
+    <div className="search-container">
+        <input type="text" placeholder="Buscar gifs"/>
+        <button>Buscar</button>
+    </div>
+    {/* Busquedas previas */}
+    <div className="previous-searches">
+        <h2>Busquedas previas</h2>
+        <ul className="previous-searches-list">
+            <li>Goku</li>
+            <li>Saitama</li>
+            <li>Elden Ring</li>
+        </ul>
+    </div>
     </>
 )
 }
