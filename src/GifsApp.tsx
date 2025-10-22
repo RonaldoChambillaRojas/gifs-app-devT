49f2cfb988acae3af2d4756908f3fea68691ba04
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-21 23:05:16 -0500

    feat: (index.html, src/main.tsx, src/GifsApp.tsx) add new features

---
+++ src/GifsApp.tsx
@@ -0,0 +1,13 @@
+export const GifsApp = () => {
+  return (
+    <>
+    {/* Header */}
+
+    <div>
+        <h1>Buscador de Gifs</h1>
+        <p>Descubre y comparte el gif perfecto</p>
+    </div>
+    
+    </>
+)
+}
