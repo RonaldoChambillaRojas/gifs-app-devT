eed0baa966ed273763cc9a3c21525e2dba2a6e96
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-23 00:05:42 -0500

    fix: fix some issues

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react"
 import { GifList } from "./gifs/components/GifList"
 import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
 
+
 export const GifsApp = () => {
+    const [previousTems, setPreviousTerms] = useState(['dragon ball z']);
+
+    const handleTermClicked = ( term: string ) => {
+        console.log({term});
+    }
   return (
     <>
     <CustomHeader title= "Buscador de Gifs" description="Descubre y comparte el gif" />
     <SearchBar placeholder="Busca lo que quieras" />
-    <PreviousSearches searches={['Goku','Dragon Ball Z']}/>
+    <PreviousSearches searches={previousTems} onLabelClick={handleTermClicked} />
     <GifList gifs={mockGifs}/>
     </>
 )
--- src/gifs/components/PreviousSearches.tsx
+++ src/gifs/components/PreviousSearches.tsx
@@ -1,16 +1,18 @@
 
 interface Props {
   searches: string[];
+
+  onLabelClick: (term: string) => void;
 }
 
-export const PreviousSearches = ({searches}: Props) => {
+export const PreviousSearches = ({searches, onLabelClick}: Props) => {
   return (
         <div className="previous-searches">
             <h2>Busquedas previas</h2>
             <ul className="previous-searches-list">
               {
                 searches.map(search => (
-                    <li key={search}>{search}</li>
+                    <li onClick={() => onLabelClick(search)} key={search}>{search}</li>
                 ))
               }
             </ul>
